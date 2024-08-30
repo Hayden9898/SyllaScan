754f41a83c37035c93efd469afbc93d2817bc5d1
Peterson Guo <petersonguo@gmail.com>
2024-08-30 17:06:13 -0400

    Google Picker

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -12,4 +16,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("home.html", {"request": request, "API_KEY": os.getenv("API_KEY"), "CLIENT_SECRET": os.getenv("CLIENT_SECRET"), "CLIENT_ID": os.getenv("CLIENT_ID")})
