3156433fc2200d225b971259332f79bba116b5b5
Peterson Guo <petersonguo@gmail.com>
2024-09-05 13:29:10 -0400

    fix file processing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -16,4 +16,37 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "API_KEY": os.getenv("API_KEY"), "CLIENT_SECRET": os.getenv("CLIENT_SECRET"), "CLIENT_ID": os.getenv("CLIENT_ID")})
+    return templates.TemplateResponse("home.html", {"request": request, "API_KEY": os.getenv("API_KEY"), "CLIENT_SECRET": os.getenv("CLIENT_SECRET"), "CLIENT_ID": os.getenv("CLIENT_ID"), "APP_ID": os.getenv("APP_ID")})
+
+# need to accept only .txt, .docx, .pdf files
+@app.post("/upload", response_model=dict)
+async def upload(request: Request):
+    form = await request.form()
+    file = None
+    contents = None
+
+    if "file" in form:
+        # Handle file upload scenario
+        file = form["file"]  # FileUpload object from form
+        if file:
+            if not (
+                file.filename.endswith(".txt")
+                or file.filename.endswith(".docx")
+                or file.filename.endswith(".pdf")
+            ):
+                raise HTTPException(status_code=415, detail="File type not supported")
+            contents = await file.read()
+        else:
+            raise HTTPException(status_code=400, detail="No valid file part found")
+    else:
+        # No URL or file provided
+        raise HTTPException(status_code=400, detail="No file or URL provided")
+
+    # process with ai here
+    make_calendar_events()
+
+    print(contents)
+    return {"filename": file.filename}
+
+def make_calendar_events():
+    pass
