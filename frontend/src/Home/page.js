355503dd71277d4604b57e44cfc58dc32b20a64d
Hayden <haydenc9898@gmail.com>
2024-12-30 18:30:53 -0500

    Changed UI layout

--- frontend/src/Home/page.js
+++ frontend/src/Home/page.js
@@ -3,19 +3,32 @@ import Button from 'components/Button'
 export default function Home({ setScreen }) {
     return (
         <>
-            <div className='wrapper-container'>
-                <div className="about-container" style={{ width: 500 }}>
-                    <div className="text-container">
-                        <div className="title">Syllabus Scanner</div>
-                        <i><div className="slogan-text">"Conquering Procrastination, one deadline at a time."</div></i>
-                        <p className="about-text" >
-                            The purpose of this application is to make scheduling important events in a student's course syllabus to ensure you never miss an important
-                            assignment, quiz, test, mid-term, or any important class event. We also strive to give you constant reminders to start studying for tests
-                            or complete assignments early, so you are always prepared.
-                        </p>
+            <div className='header-columns'>
+                <div className='title-col'>
+                    <div className="title">Syllabus Scanner
+                            <i><div className="slogan-text">"Conquering Procrastination, one deadline at a time."</div></i>
+                        </div>
                     </div>
+                <div className='title-col'>
+                    
                 </div>
             </div>
+            <svg className='wave' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <defs>
+                        <pattern id="imagePattern" patternUnits="userSpaceOnUse" width="100%" height="100%">
+                            <image
+                                xlinkHref='https://preview.redd.it/3tgcrfk5o5t11.png?width=1920&format=png&auto=webp&s=a46b80d727f91d126ef03c80565045f73eb72b7c'
+                                x="0"
+                                y="0"
+                                width="1440"
+                                height="320"
+                                preserveAspectRatio="none"
+                            />
+                        </pattern>
+                    </defs>
+                    <path fill="url(#imagePattern)" fill-opacity="1" d="M0,288L60,266.7C120,245,240,203,360,197.3C480,192,600,224,720,240C840,256,960,256,1080,250.7C1200,245,1320,235,1380,229.3L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z">
+                    </path>
+                </svg>
             {/*implement about us*/}
             <div className='wrapper-container'>
                 <div className='mx-auto justify-self-center'>
