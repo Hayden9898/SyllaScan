0b28160322f0ae0aa043056ae186265f3699be6f
Hayden9898 <haydenc9898@gmail.com>
2025-01-10 16:56:26 -0500

    Fixed background for upload page

--- frontend/src/DrivePicker/DrivePicker.jsx
+++ frontend/src/DrivePicker/DrivePicker.jsx
@@ -23,49 +23,58 @@ export default function DrivePicker({
 	}, [localFiles]);
 
 	return (
-		<div className="">
-			<div className="d-flex flex-column justify-content-center">
-				<FilePreview
-					fileLinks={fileLinks}
-					localFiles={localFiles}
-					setFileLinks={setFileLinks}
-					setLocalFiles={setLocalFiles}
-				/>
-				<ButtonGroup
-					setLocalFiles={setLocalFiles}
-					authToken={authToken}
-					setFileLinks={setFileLinks}
-					fileLinks={fileLinks}
-					setAuthToken={setAuthToken}
-					className={"absolute top-[75%]"}
-				/>
-			</div>
-			{(fileLinks.length > 0 || localFiles.length > 0) && (
-				<div className="continue-button-container">
-					<button
-						style={{
-							position: "relative",
-							display: "inline-block",
-							cursor: "pointer",
-							outline: "none",
-							border: 0,
-							verticalAlign: "middle",
-							textDecoration: "none",
-							background: "transparent",
-							padding: 0,
-							fontSize: "inherit",
-							fontFamily: "inherit",
-						}}
-						className="continue"
-						onClick={() => setScreen("export")}
-					>
-						<span className="circle" aria-hidden="true">
-							<span className="icon arrow"></span>
-						</span>
-						<span className="button-text">Continue</span>
-					</button>
+		<div className="background">
+			<div className="">
+				<div className="d-flex flex-column justify-content-center">
+					<div className="upload-file-label">
+						<h1>Get Started: Upload your syllabus via Local or Google Drive!
+						</h1>
+						<p>
+							Let us do the organizing for you
+						</p>
+					</div>
+					<FilePreview
+						fileLinks={fileLinks}
+						localFiles={localFiles}
+						setFileLinks={setFileLinks}
+						setLocalFiles={setLocalFiles}
+					/>
+					<ButtonGroup
+						setLocalFiles={setLocalFiles}
+						authToken={authToken}
+						setFileLinks={setFileLinks}
+						fileLinks={fileLinks}
+						setAuthToken={setAuthToken}
+						className={"absolute top-[75%]"}
+					/>
 				</div>
-			)}
+				{(fileLinks.length > 0 || localFiles.length > 0) && (
+					<div className="continue-button-container">
+						<button
+							style={{
+								position: "relative",
+								display: "inline-block",
+								cursor: "pointer",
+								outline: "none",
+								border: 0,
+								verticalAlign: "middle",
+								textDecoration: "none",
+								background: "transparent",
+								padding: 0,
+								fontSize: "inherit",
+								fontFamily: "inherit",
+							}}
+							className="continue"
+							onClick={() => setScreen("export")}
+						>
+							<span className="circle" aria-hidden="true">
+								<span className="icon arrow"></span>
+							</span>
+							<span className="button-text">Continue</span>
+						</button>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
