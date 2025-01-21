9df69fd953556d3f3dc8dbe61697b589e1b10c3d
Hayden9898 <haydenc9898@gmail.com>
2025-01-21 18:18:22 -0500

    improved processsing UI page

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -2,14 +2,16 @@ import 'css/LoadingScreen.css';
 
 export default function Loader({message}) {
     return (
-        <div className="center-components">
-            <div className="lds-ring">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+        <div className="loading-background">
+            <div className="center-components">
+                <div className="lds-ring">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <h1 className="processing-title">{message}</h1>
             </div>
-            <h1 className="processing-title">{message}</h1>
         </div>
     );
 };
